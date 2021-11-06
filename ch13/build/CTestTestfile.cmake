ca3c1e68bb421dbffe4e9f8b25c3a01b831452ea
e <xieyin@bupt.edu.cn>
2021-11-06 17:56:10 +0800

    fix:fix bug in origin source code

--- ch13/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xwell/Documents/slambook2/ch13
-# Build directory: /home/xwell/Documents/slambook2/ch13/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
-subdirs("app")
